cfbb41ee11c93d35dd0b27d1318eaed8733654d3
KALADHAR GA <hikaladhar@gmail.com>
2025-09-26 17:41:39 +0530

    Delete fcp_program_12.c

--- fcp_program_12.c
+++
@@ -1,9 +0,0 @@
-//ascii code of any character
-#include<stdio.h> 
-int main(){ 
-char s1; 
-printf("\nEnter any character:"); 
-scanf("%c",&s1); 
-printf("Ascii code of %c is %d",s1,s1); 
-return 0; 
-}
